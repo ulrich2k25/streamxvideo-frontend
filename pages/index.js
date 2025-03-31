b76066f7b3ec4201ca6a71c566882531dfa4a9c4
ulrich2k25 <ulrichpouke150@gmail.com>
2025-03-31 18:54:05 +0200

    ✅ Ajout de Tailwind CSS et activation du design premium

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 // ✅ VERSION FONCTIONNELLE ET LUXUEUSE
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import './index.css';
+
 
 const backendUrl = "https://streamxvideo-backend-production.up.railway.app";
 
